ebfbf5cc025b53ae354a20a94b33bf9b858f2a8f
Troy <115662625+troy-c137@users.noreply.github.com>
2022-10-16 18:07:46 -0400

    Update and rename Taxes to TaxReturn.java

--- TaxReturn.java
+++ TaxReturn.java
@@ -102,35 +102,3 @@ public class TaxReturn {
     return taxOwed;
     }
 }
-
-/**
- * ------------------------BREAK-------------------------
- */
- 
- import java.util.Scanner;
-
-/**
- * This calculates an income tax return using 2022-2023 rates.
- */
-
-public class TaxCalculator {
-
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Are you married? (Y/N): ");
-        String answer = input.next();
-
-        System.out.print("Please enter your income: ");
-        double income = input.nextDouble();
-
-        int status;
-        if (answer.equals("Y")){
-            status = TaxReturn.MARRIED;
-        } else {
-            status = TaxReturn.SINGLE;
-        }
-        TaxReturn aTaxReturn = new TaxReturn(income, status);
-        System.out.println("Tax: " + aTaxReturn.getTax());
-    }
-}
